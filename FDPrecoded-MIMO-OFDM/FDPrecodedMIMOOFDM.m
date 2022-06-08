31f2c90623e867e72daf0c5c5c875f3a867f24ea
Sidong Guo <82831509+SiDongG@users.noreply.github.com>
2022-06-08 14:43:37 -0400

    Add files via upload

---
+++ FDPrecoded-MIMO-OFDM/FDPrecodedMIMOOFDM.m
@@ -0,0 +1,58 @@
+%% Frequency-Domain Precoded MIMO OFDM
+clear; clc; close all;
+%% Parameter Initialization
+% Standard: MMSE Precoding, Complex Orthogonal STBC, 4QAM, TCSI
+Tx=4; %Number of Transmit Antenna
+Rx=6; %Number of Receive Antenna 
+L=4;  %Channel Length
+C=4;  %CP Length
+M=4;  %4-QAM
+N=64; %Block Size
+P=N+C;
+Block_Num=Tx*10;
+%% Mapping 
+Bits=randi(0:1,[1,N*Block_Num*log2(M)]);
+Bits2=zeros(1,length(Bits)/log2(M));
+for i=1:log2(M):length(Bits)
+    Bits2(1+(i-1)/log2(M))=bin2dec(num2str(Bits(i:i+log2(M)-1)));
+end
+if M==4
+    Bits3=qammod(Bits2,M)*sqrt(0.5);
+end
+%% Carrier-wise FFT
+Symbol=reshape(Bits3,N,1,Block_Num);
+FFT=dftmtx(N)/sqrt(N);
+IFFT=conj(FFT);
+Symbol1=zeros(size(Symbol));
+for count=1:Block_Num
+    Symbol1(:,:,count)=FFT*Symbol(:,:,count);
+end
+%% Channel Precoding
+%Generate MIMO channel matrix, which is a four-dimensional matrix 
+HH=zeros(N,N,Tx,Rx);
+DD=zeros(N,N,Tx,Rx);
+for i=1:Tx
+    for j=1:Rx
+        h=(1/sqrt(2*L))*(randn(1,L)+1i*randn(1,L));
+        a=1;
+        H=zeros(N+L-1,N);
+        for k=1:N+L-1
+            for m=1:N
+                if k-m+1>L
+                    H(k,m)=0;
+                elseif k-m<0
+                    H(k,m)=0;
+                else
+                    H(k,m)=h(k-m+1);
+                end
+            end
+        end
+        H(1:L-1,:)=H(1:L-1,:)+H(N+1:N+L-1,:);
+        H=H(1:N,:);
+        HH(:,:,i,j)=H;
+        DD(:,:,i,j)=FFT*H*IFFT;
+    end
+end
+
+
+
